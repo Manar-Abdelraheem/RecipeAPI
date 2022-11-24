1e566aa3353aff19bb2e580a374af4529712f1b5
Manar <manar.abd.alrahem.2000@gmail.com>
2022-11-24 02:03:17 +0100

    Add project files.

---
+++ Recipe-API/Controllers/RecipeAPIController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Recipe_API.Models;
+
+namespace Recipe_API.Controllers
+{
+    [Route("api/RecipeAPI")]
+    [ApiController]
+    public class RecipeAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Recipe> GetRecipes() 
+        {
+            return new List<Recipe>
+            {
+                new Recipe{ Id=1,Title="molokhia"},
+                new Recipe{ Id=2,Title="bamia"}
+            };
+        }
+
+    }
+}
---
+++ Recipe-API/Models/Recipe.cs
@@ -0,0 +1,11 @@
+﻿namespace Recipe_API.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Ingredients { get; set; }
+        public string Instructions { get; set; }
+        public string Categories { get; set; }
+    }
+}
